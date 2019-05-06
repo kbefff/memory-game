b0f6a8574d6a007b859f56fe5a301032695667be
kbefff <kbeffel10@me.com>
2019-05-06 12:36:58 -0700

    navbar

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,1 +1,15 @@
-export {default} from "./FriendCard";
+import React from "react";
+import "./style.css";
+
+const CharacterCard = props => (
+    <div
+        className="card"
+        value={props.id}
+        onClick={() => props.handleClick(props.id)}>
+        <div className="img-container">
+            <img alt={props.name} src={props.image}/>
+        </div>
+    </div>
+);
+
+export default CharacterCard;
