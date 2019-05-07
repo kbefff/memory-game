86df0a31ced7ca7a7db1c34f8de23fa2642a2ceb
kbefff <kbeffel10@me.com>
2019-05-07 14:20:38 -0700

    Updates

---
+++ precache-manifest.15294337d32aeb2bb34316e6f6eec0cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3c69a8cfad19af610b800094bebcdb",
+    "url": "/kbefff/memory-game/index.html"
+  },
+  {
+    "revision": "9891e695286fb093b5a1",
+    "url": "/kbefff/memory-game/static/css/main.f9dd473b.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/kbefff/memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9891e695286fb093b5a1",
+    "url": "/kbefff/memory-game/static/js/main.7648e0e9.chunk.js"
+  },
+  {
+    "revision": "25ed70383f3b422dc064",
+    "url": "/kbefff/memory-game/static/js/runtime~main.35075070.js"
+  }
+]);
